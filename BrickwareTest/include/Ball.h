1a7cc596ae7679414dabf2939fd9c505830d9376
unknown <gap8240@GDD-54.main.ad.rit.edu>
2015-04-08 22:47:07 -0400

    Added method stubs

--- BrickwareTest/include/Ball.h
+++ BrickwareTest/include/Ball.h
@@ -8,18 +8,27 @@ using namespace Core;
 using namespace Math;
 using namespace Graphics;
 
+enum SelectionState{
+	HIGHLIGHTED,
+	SELECTED,
+	NONE
 
+};
 class Ball
 {
 
 private:
 	Material* color;
 	Dimple* currentDimple;
+	SelectionState state;
 	// board will have a list of ALL BALLS ON THE BAORD apaprently and which ball is currently selected
-	//Board* board;
+	Board* board;
 
 public:
 	Ball();
 	~Ball();
+	
+	Ball(Material* c, Dimple* startDimple, Board* b);
+	
 };
 
--- BrickwareTest/include/Dimple.h
+++ BrickwareTest/include/Dimple.h
@@ -1,6 +1,34 @@
 #pragma once
+#include "Dimple.h"
+#include "Ball.h"
+#include <list>
+
+using namespace Brickware;
+using namespace Core;
+using namespace Math;
+using namespace Graphics;
+
+enum Direction{
+	LEFT = 0,
+	UP_LEFT = 1,
+	UP_RIGHT = 2,
+	RIGHT = 3,
+	DOWN_RIGHT = 4,
+	DOWN_LEFT = 5
+};
+
+struct Neighbor{
+	Dimple dimple;
+	Direction direction;
+
+};
+
 class Dimple
 {
+
+private:
+	Board* board;
+	std::list<Neighbor> neighbors;
 public:
 	Dimple();
 	~Dimple();
